6fa557c1cc417361c5035d395ab692d1cbf0b0da
zhaoyanbin <zhaoyb960520@163.com>
2020-12-30 08:32:28 +0800

    修改了第一个视图函数和注册函数

--- userapp/views.py
+++ userapp/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def index(requext):
+def index(request):
     print('第一个视图函数')
     return HttpResponse('OK')
 
-def register(requext):
+def register(request):
     return HttpResponse('注册成功')
+
+
